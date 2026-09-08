6409b934c11f32166f9dfc62884073d86c9d3f9c
eduardoblasczak <eduardo.blasczak@gmail.com>
2026-09-08 18:39:37 -0300

    Cafeteria - Bebida cappuccino

---
+++ ResolucaoDeProblemas/src/Cafeteria/Cappuccino.java
@@ -0,0 +1,14 @@
+package Cafeteria;
+
+public class Cappuccino extends Bebida {
+
+    @Override
+    public String getDescricao() {
+        return "Cappuccino";
+    }
+
+    @Override
+    public double getPreco() {
+        return 10.50;
+    }
+}
