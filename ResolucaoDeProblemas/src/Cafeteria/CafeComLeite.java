dc00169d7e87fccc1245df3da530f8fbb4b578cb
eduardoblasczak <eduardo.blasczak@gmail.com>
2026-09-08 18:39:37 -0300

    Cafeteria - Bebida cafe com leite

---
+++ ResolucaoDeProblemas/src/Cafeteria/CafeComLeite.java
@@ -0,0 +1,14 @@
+package Cafeteria;
+
+public class CafeComLeite extends Bebida {
+
+    @Override
+    public String getDescricao() {
+        return "Cafe com leite";
+    }
+
+    @Override
+    public double getPreco() {
+        return 8.00;
+    }
+}
