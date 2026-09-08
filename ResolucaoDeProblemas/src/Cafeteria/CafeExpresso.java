c29df9533ff9f90c98fb0a680128b229003b2b05
eduardoblasczak <eduardo.blasczak@gmail.com>
2026-09-08 18:39:37 -0300

    Cafeteria - Bebida cafe expresso

---
+++ ResolucaoDeProblemas/src/Cafeteria/CafeExpresso.java
@@ -0,0 +1,14 @@
+package Cafeteria;
+
+public class CafeExpresso extends Bebida {
+
+    @Override
+    public String getDescricao() {
+        return "Cafe expresso";
+    }
+
+    @Override
+    public double getPreco() {
+        return 6.00;
+    }
+}
