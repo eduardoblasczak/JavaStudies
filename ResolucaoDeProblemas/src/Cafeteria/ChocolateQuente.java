b7605ce14df16e8d3419fe1d7d2e707fb1ed9557
eduardoblasczak <eduardo.blasczak@gmail.com>
2026-09-08 18:39:37 -0300

    Cafeteria - Bebida chocolate quente

---
+++ ResolucaoDeProblemas/src/Cafeteria/ChocolateQuente.java
@@ -0,0 +1,14 @@
+package Cafeteria;
+
+public class ChocolateQuente extends Bebida {
+
+    @Override
+    public String getDescricao() {
+        return "Chocolate quente";
+    }
+
+    @Override
+    public double getPreco() {
+        return 9.00;
+    }
+}
